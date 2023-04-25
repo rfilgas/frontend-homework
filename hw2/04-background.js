8024432f2427d5b334ea201291c21422c6063bab
Ryan Filgas <rfilgas@pdx.edu>
2023-04-25 01:03:02 -0700

    complete exercises 3-5

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,63 @@
 // Add your code here
+let numSeconds = 3000;
+let nIntervId;
+const button = document.getElementById('toggle-button');
+
+function getNumInRange(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function flashColor() {
+  const pageColor = document.getElementById('body-color');
+  const temp = 'background-color:';
+  const red = getNumInRange(200, 230);
+  const green = getNumInRange(200, 230);
+  const blue = getNumInRange(200, 230);
+  const alpha = getNumInRange(240, 230);
+  pageColor.style = temp.concat('rgba(', red, ',', blue, ',', green, ',', alpha, ');');
+}
+
+function start() {
+  button.classList.remove('btn-primary');
+  button.classList.add('btn-danger');
+  if (!nIntervId) {
+    nIntervId = setInterval(flashColor, numSeconds);
+  }
+  return 'Stop';
+}
+
+function stop() {
+  button.classList.remove('btn-danger');
+  button.classList.add('btn-primary');
+  clearInterval(nIntervId);
+  nIntervId = null;
+  return 'Start';
+}
+
+function handleInput() {
+  if (document.getElementById('num-input').value !== '') {
+    numSeconds = document.getElementById('num-input').value * 1000;
+  }
+  if (nIntervId) {
+    clearInterval(nIntervId);
+    nIntervId = null;
+    nIntervId = setInterval(flashColor, numSeconds);
+  }
+}
+
+button.addEventListener('click', () => {
+  button.textContent = button.textContent === 'Stop' ? stop() : start();
+});
+
+window.addEventListener('load', () => {});
+window.onload = () => {
+  start();
+};
+
+const input = document.querySelector('input');
+input.addEventListener('input', handleInput);
+
+document.getElementById('color-change-card').style.padding = '15px';
+document.getElementById('color-change-card').overflow = 'auto';
+document.getElementById('color-change-card').style.position = 'relative';
+document.getElementById('color-change-card').style.top = '30px';
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,18 @@
-// Add your code here
+document.getElementById('form-container').style.padding = '15px';
+document.getElementById('form-container').overflow = 'auto';
+document.getElementById('form-container').style.position = 'relative';
+document.getElementById('form-container').style.top = '30px';
+
+function logSubmit(event) {
+  console.log('Full Name:', document.getElementById('input-full-name').value);
+  console.log('Email:', document.getElementById('input-email').value);
+  console.log('Registration Status:', document.getElementById('registration-status').value);
+  console.log('Programming Languages:', document.getElementById('programming-language-check').checked);
+  console.log('Operating Systems:', document.getElementById('operating-systems-check').checked);
+  console.log('Full Stack:', document.getElementById('full-stack-check').checked);
+  console.log('Anything Else:', document.getElementById('anything-else-box').value);
+  event.preventDefault();
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', logSubmit);

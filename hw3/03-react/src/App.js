3c3fa7b72ed837b16d3774053edcd84c49d69005
Ryan Filgas <rfilgas@pdx.edu>
2023-05-07 23:23:15 -0700

    complete part 3

---
+++ hw3/03-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./navigation/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
